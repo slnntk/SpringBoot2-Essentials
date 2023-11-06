bc9bf5b50dcd201bf8f03ec1a0fb2d2694a89c87
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-11-06 13:57:33 -0300

    first commit

---
+++ src/main/java/com/example/springboot2essentials/SpringBoot2EssentialsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot2essentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2EssentialsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2EssentialsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot2essentials/SpringBoot2EssentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot2essentials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2EssentialsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
