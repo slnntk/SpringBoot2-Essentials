0377985f8c267fa6010a9187befd71c864d8e779
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-11-16 14:47:35 -0300

    Spring Boot 2 Essentials 13 - Spring Data JPA pt 01
    @Entity
    @NoArgsArgument
    
    spring:
      datasource:
        url: jdbc:mysql://localhost:3306/anime?useSSL=false&createDatabaseIfNotExist=true&allowPublicKeyRetrieval=true
        username: root
        password: root
      jpa:
        hibernate:
          ddl-auto: update

--- src/main/java/study/devdojo/springboot2essentials/domain/Anime.java
+++ src/main/java/study/devdojo/springboot2essentials/domain/Anime.java
@@ -3,11 +3,22 @@ package study.devdojo.springboot2essentials.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 @Data // Gerar get, set, equals e hashcode
 @AllArgsConstructor
+@NoArgsConstructor
 public class Anime {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 }
