134193d327b828caa0ceed5259b794d1c77d0ba5
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-12-20 15:40:08 -0300

    Spring Boot 2 Essentials 22 - Sobrescrevendo handler do Spring
    handleExceptionInternal
    extends ResponseEntityExceptionHandler

--- src/main/java/study/devdojo/springboot2essentials/handler/RestExceptionHandler.java
+++ src/main/java/study/devdojo/springboot2essentials/handler/RestExceptionHandler.java
@@ -1,12 +1,16 @@
 package study.devdojo.springboot2essentials.handler;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import study.devdojo.springboot2essentials.exception.*;
 
 import java.time.LocalDateTime;
@@ -21,40 +25,73 @@ import java.util.stream.Collectors;
  */
 @ControllerAdvice
 @Log4j2
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     /**
-     * Método para lidar com a exceção BadRequestException.
+     * Método handleBadRequestException.
+     * Este método trata exceções do tipo BadRequestException, gerando uma resposta com detalhes específicos
+     * da exceção BadRequestException. Retorna um ResponseEntity contendo detalhes da exceção, como timestamp,
+     * status HTTP, título, detalhes, mensagem do desenvolvedor, entre outros, e o código de status HTTP 400 (Bad Request).
      *
-     * @param badRequestException Exceção BadRequestException lançada na aplicação.
-     * @return ResponseEntity contendo os detalhes da exceção e o código de status HTTP BAD_REQUEST.
-     * Este método trata exceções do tipo BadRequestException, retornando um ResponseEntity com os detalhes
-     * da exceção, como título, detalhes, mensagem do desenvolvedor, entre outros, e o código de status HTTP 400.
+     * @param bre Exceção BadRequestException lançada na aplicação.
+     * @return ResponseEntity contendo os detalhes da exceção BadRequestException e código de status HTTP 400.
      */
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestException(BadRequestException badRequestException) {
+    public ResponseEntity<BadRequestExceptionDetails> handleBadRequestException(BadRequestException bre) {
+        // Lógica para construir e retornar um ResponseEntity com detalhes específicos da BadRequestException
+        // Timestamp, status HTTP, título, detalhes, mensagem do desenvolvedor são encapsulados em um objeto BadRequestExceptionDetails
         return new ResponseEntity<>(
                 BadRequestExceptionDetails.builder()
                         .timestamp(LocalDateTime.now())
                         .status(HttpStatus.BAD_REQUEST.value())
                         .title("Bad Request Exception, Check the Documentation")
-                        .details(badRequestException.getMessage())
-                        .developerMessage(badRequestException.getClass().getName())
+                        .details(bre.getMessage())
+                        .developerMessage(bre.getClass().getName())
                         .build(), HttpStatus.BAD_REQUEST);
     }
 
+
     /**
-     * Método para lidar com a exceção MethodArgumentNotValidException.
+     * Método handleObjectNotFoundException.
+     * Este método trata exceções do tipo ObjectNotFoundException, gerando uma resposta com detalhes específicos
+     * da exceção ObjectNotFoundException. Retorna um ResponseEntity contendo detalhes da exceção, como timestamp,
+     * status HTTP, título, detalhes, mensagem do desenvolvedor, entre outros, e o código de status HTTP 404 (Not Found).
      *
-     * @param methodArgumentNotValidException Exceção MethodArgumentNotValidException lançada na aplicação.
-     * @return ResponseEntity contendo os detalhes da exceção e o código de status HTTP BAD_REQUEST.
-     * Este método trata exceções do tipo MethodArgumentNotValidException, retornando um ResponseEntity com os detalhes
-     * da exceção, como título, detalhes, campos inválidos, mensagem do desenvolvedor, entre outros,
-     * e o código de status HTTP 400.
+     * @param objectNotFoundException Exceção ObjectNotFoundException lançada na aplicação.
+     * @return ResponseEntity contendo os detalhes da exceção ObjectNotFoundException e código de status HTTP 404.
      */
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ValidationExceptionDetails> handlerMethodArgumentNotValidException(MethodArgumentNotValidException methodArgumentNotValidException) {
-        List<FieldError> fieldErrors = methodArgumentNotValidException.getBindingResult().getFieldErrors();
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<ObjectNotFoundExceptionDetails> handleObjectNotFoundException(ObjectNotFoundException objectNotFoundException) {
+        // Lógica para construir e retornar um ResponseEntity com detalhes específicos da ObjectNotFoundException
+        // Timestamp, status HTTP, título, detalhes, mensagem do desenvolvedor são encapsulados em um objeto ObjectNotFoundExceptionDetails
+        return new ResponseEntity<>(
+                ObjectNotFoundExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.NOT_FOUND.value())
+                        .title("Object Not Found Exception")
+                        .details(objectNotFoundException.getMessage())
+                        .developerMessage(objectNotFoundException.getClass().getName())
+                        .build(), HttpStatus.NOT_FOUND);
+    }
+
+
+    /**
+     * Método handleMethodArgumentNotValid.
+     * Este método trata exceções do tipo MethodArgumentNotValidException, gerando uma resposta com detalhes
+     * sobre campos inválidos nos dados da requisição. Retorna um ResponseEntity contendo detalhes sobre os campos
+     * inválidos, como timestamp, status HTTP, título, detalhes, mensagem do desenvolvedor, entre outros, e o
+     * código de status HTTP 400 (Bad Request).
+     *
+     * @param exception Exceção MethodArgumentNotValidException lançada na aplicação.
+     * @param headers   Cabeçalhos da requisição.
+     * @param status    Status HTTP.
+     * @param request   Requisição.
+     * @return ResponseEntity contendo detalhes sobre campos inválidos e código de status HTTP 400.
+     */
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
 
         String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(", "));
         String fieldsMessage = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(", "));
@@ -65,29 +102,37 @@ public class RestExceptionHandler {
                         .status(HttpStatus.BAD_REQUEST.value())
                         .title("Bad Request Exception, Invalid Fields")
                         .details("Check the field(s) error")
-                        .developerMessage(methodArgumentNotValidException.getClass().getName())
+                        .developerMessage(exception.getClass().getName())
                         .fields(fields)
                         .fieldsMessage(fieldsMessage)
                         .build(), HttpStatus.BAD_REQUEST);
     }
 
     /**
-     * Método para lidar com a exceção ObjectNotFoundException.
+     * Método handleExceptionInternal.
+     * Este método trata exceções internas, gerando uma resposta com detalhes gerais da exceção interna. Retorna
+     * um ResponseEntity contendo detalhes gerais da exceção, como timestamp, status HTTP, título, detalhes, mensagem
+     * do desenvolvedor, entre outros, e o código de status HTTP apropriado.
      *
-     * @param objectNotFoundException Exceção ObjectNotFoundException lançada na aplicação.
-     * @return ResponseEntity contendo os detalhes da exceção e o código de status HTTP NOT_FOUND.
-     * Este método trata exceções do tipo ObjectNotFoundException, retornando um ResponseEntity com os detalhes
-     * da exceção, como título, detalhes, mensagem do desenvolvedor, entre outros, e o código de status HTTP 404.
+     * @param ex       Exceção interna gerada pelo sistema.
+     * @param body     Corpo da resposta.
+     * @param headers  Cabeçalhos da requisição.
+     * @param status   Status HTTP.
+     * @param request  Requisição.
+     * @return ResponseEntity contendo detalhes gerais da exceção interna e código de status HTTP apropriado.
      */
-    @ExceptionHandler(ObjectNotFoundException.class)
-    public ResponseEntity<ObjectNotFoundExceptionDetails> handlerObjectNotFoundException(ObjectNotFoundException objectNotFoundException) {
-        return new ResponseEntity<>(
-                ObjectNotFoundExceptionDetails.builder()
-                        .timestamp(LocalDateTime.now())
-                        .status(HttpStatus.NOT_FOUND.value())
-                        .title("Object Not Found Exception")
-                        .details(objectNotFoundException.getMessage())
-                        .developerMessage(objectNotFoundException.getClass().getName())
-                        .build(), HttpStatus.NOT_FOUND);
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        ExceptionDetails exceptionDetails = ExceptionDetails.builder()
+                .timestamp(LocalDateTime.now())
+                .status(status.value())
+                .title(ex.getCause().getMessage())
+                .details(ex.getMessage())
+                .developerMessage(ex.getClass().getName())
+                .build();
+
+        return new ResponseEntity<>(exceptionDetails, headers, status);
     }
+
 }
